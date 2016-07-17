4e3e33c85b1cf1ca7a380abbd807c9bd89d7a383
djonesembedded <davidjones@sportronics.com.au>
2016-07-17 23:42:46 +1000

    Second commit

---
+++ HubRegister/AzureIoTHub.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Devices.Tpm;
+using Microsoft.Azure.Devices.Client;
+
+static class AzureIoTHub
+{
+    //
+    // This sample assumes the device has been connected to Azure with the IoT Dashboard
+    //
+    // Refer to http://aka.ms/azure-iot-hub-vs-cs-wiki for more information on Connected Service for Azure IoT Hub
+
+        //Not Using TPM:
+    //private static deviceConnectionString = 
+    //"HostName=<HostName.usr.azure-devices.net;DeviceId=MyDevice;SharedAccessKey=XXXXXX";
+
+
+    public static async Task SendDeviceToCloudMessageAsync()
+    {
+        //Using TPM
+        TpmDevice myDevice = new TpmDevice(0); // Use logical device 0 on the TPM
+        string hubUri = myDevice.GetHostName();
+        string deviceId = myDevice.GetDeviceId();
+        string sasToken = myDevice.GetSASToken();
+
+        var deviceClient = DeviceClient.Create(
+            hubUri,
+            AuthenticationMethodFactory.
+                CreateAuthenticationWithToken(deviceId, sasToken), TransportType.Amqp);
+
+        //Not using TPM
+        //var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Amqp);
+
+
+#if WINDOWS_UWP
+        var str = "Hello, Cloud from a secure UWP C# app!";
+#else
+        var str = "Hello, Cloud from a secure C# app!";
+#endif
+        var message = new Message(Encoding.ASCII.GetBytes(str));
+
+        await deviceClient.SendEventAsync(message);
+    }
+
+    public static async Task SendDeviceToCloudMessageAsync(string msg)
+    {
+        TpmDevice myDevice = new TpmDevice(0); // Use logical device 0 on the TPM
+        string hubUri = myDevice.GetHostName();
+        string deviceId = myDevice.GetDeviceId();
+        string sasToken = myDevice.GetSASToken();
+
+        var deviceClient = DeviceClient.Create(
+            hubUri,
+            AuthenticationMethodFactory.
+                CreateAuthenticationWithToken(deviceId, sasToken), TransportType.Amqp);
+
+        //Not using TPM
+        //var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Amqp);
+
+        var message = new Message(Encoding.ASCII.GetBytes(msg));
+
+        await deviceClient.SendEventAsync(message);
+    }
+
+    public static async Task<string> ReceiveCloudToDeviceMessageAsync()
+    {
+        TpmDevice myDevice = new TpmDevice(0); // Use logical device 0 on the TPM by default
+        string hubUri = myDevice.GetHostName();
+        string deviceId = myDevice.GetDeviceId();
+        string sasToken = myDevice.GetSASToken();
+
+        //Use TPM
+        var deviceClient = DeviceClient.Create(
+            hubUri,
+            AuthenticationMethodFactory.
+                CreateAuthenticationWithToken(deviceId, sasToken), TransportType.Amqp);
+
+        //Not using TPM
+        //var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Amqp);
+
+        while (true)
+        {
+            var receivedMessage = await deviceClient.ReceiveAsync();
+
+            if (receivedMessage != null)
+            {
+                var messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                await deviceClient.CompleteAsync(receivedMessage);
+                return messageData;
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
---
+++ HubRegister/MainPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HubRegister
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            textBoxRecv.Text = "";
+            SendIsRunning = false;
+            RecvIsRunning = false;
+        }
+
+        private async void buttonSend_Click(object sender, RoutedEventArgs e)
+        {
+            await AzureIoTHub.SendDeviceToCloudMessageAsync(textBoxSend.Text);
+        }
+
+        int count = 0;
+        private async void buttonRecv_Click(object sender, RoutedEventArgs e)
+        {
+            await GetMsgs();
+        }
+
+        private async Task GetMsgs()
+        {
+            string msg = await AzureIoTHub.ReceiveCloudToDeviceMessageAsync();
+            await DisplayText(msg);
+        }
+
+
+        private async Task DisplayText(string msg)
+        {
+            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+            () =>
+            {
+                textBoxRecv.Text = (count++).ToString() +" " + msg + "\r\n" + textBoxRecv.Text;
+            });
+        }
+
+        private async Task DisplayText2(string msg)
+        {
+            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+            () =>
+            {
+                textBoxSend.Text = msg;
+            });
+        }
+
+        private bool RecvIsRunning = false;
+        private async void buttonRun_Click(object sender, RoutedEventArgs e)
+        {
+            count = 0;
+            RecvIsRunning = !RecvIsRunning;
+            while (RecvIsRunning)
+            {
+                await Task.Run(async () =>
+                {
+                    await DisplayText("Receiver is running.");
+                    while (RecvIsRunning)
+                        await GetMsgs();
+                    await DisplayText("Receiver is stopping.");
+                });
+            }
+
+        }
+
+        private bool SendIsRunning = false;
+        int num = 0;
+        private async void buttonSendRun_Click(object sender, RoutedEventArgs e)
+        {
+
+            SendIsRunning = !SendIsRunning;
+            while (SendIsRunning)
+            {
+                await Task.Run(async () =>
+                {
+                    if (num++ > 10)
+                        num = 0;
+                    await DisplayText2(num.ToString());
+                    string dt = DateTime.Now.ToString();
+                    string jsn = "{\"Time\":\"" + dt + "\"," + "\"num\":" + num.ToString() + "}";
+                    await AzureIoTHub.SendDeviceToCloudMessageAsync(jsn);
+                    await Task.Delay(TimeSpan.FromSeconds(1));
+
+                });
+            }
+
+        }
+
+
+    }
+}
+
